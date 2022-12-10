e97048a24c101cc717bf02f2d661a2a486c8913d
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2022-12-10 19:19:17 +0100

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -6,18 +6,23 @@
 /*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 22:52:18 by zelhajou          #+#    #+#             */
-/*   Updated: 2022/12/05 01:20:51 by zelhajou         ###   ########.fr       */
+/*   Updated: 2022/12/10 03:23:21 by zelhajou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# include <unistd.h>
 
-void	ft_putchr(char c);
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+void	ft_putnbr(int nb);
+void	ft_putunbr(unsigned int nbr);
+void	ft_puthex(unsigned long nbr, char c);
+void	ft_putaddr(unsigned long addr);
 
 #endif
